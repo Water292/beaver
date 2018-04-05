eeed535693a0f121cb6d09e41cd3fbaa82d243cf
Stephen McIntosh <stephenmac7@gmail.com>
2018-04-05 19:55:56 -0400

    check transaction output before actually executing

--- test/beaver.js
+++ test/beaver.js
@@ -3,7 +3,10 @@ const Beaver = artifacts.require("Beaver");
 contract("Beaver test", async (accounts) => {
   it("should be possible to buy and review a product", async () => {
     let inst = await Beaver.deployed();
-    let productId = await inst.add("Test Product", 2000000000000000);
+    let productId = await inst.add.call("Test Product", 2000000000000000);
+    assert.equal(productId, 0);
+
+    await inst.add("Test Product", 2000000000000000);
     
     await inst.buy(0, {from: accounts[1], value: 2000000000000000});
     await inst.review(0, 3, {from: accounts[1]});
